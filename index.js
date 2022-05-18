e3ae1b53074c743dc327a6e449b85cc7e901e546
Ashley Wong <tofudragon333@gmail.com>
2022-05-18 12:51:11 -0400

    commit tree 843c1e020a4602a01d038913f468540ca9caeb93

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution here
+// console.log("hello, i am dead.");
+// const list = ['Ruby', 'Sunset', 'Bob', 'Dick']
+function findMatching(drivers, name) {
+    // console.log(name);
+    let match = drivers.filter(function(pizza) {
+        //console.log(pizza);
+        return pizza.toLowerCase() === name.toLowerCase();
+    });
+    return match;
+}
+// findMatching(list, "Yennefer of Vengerberg")
+
+function fuzzyMatch(drivers, name) {
+    let kindaMatch = drivers.filter(function(pizza) {
+        return pizza.charAt(0) === name.charAt(0);
+    })
+    return kindaMatch;
+}
+
+// const list = 
+
+function matchName(drivers, name) {
+    // console.log(pizza);
+    let exactoMatcho = drivers.filter(arrayItems => arrayItems.name === name);
+    return exactoMatcho;
+}
